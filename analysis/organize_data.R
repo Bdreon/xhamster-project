dceb43b6b26105759445ba74272b781da540ef8c
Ben Dreon <bdreon@uoregeon.edu>
2019-06-05 00:02:08 -0700

    update organize data

--- analysis/organize_data.R
+++ analysis/organize_data.R
@@ -10,6 +10,7 @@
 source("useful_functions.R")
 
 library(readr)
+library(dplyr)
 
 porn <- read_csv("analysis/input/xhamster.csv.tar.gz")
 rankings <- read_csv2("analysis/input/rankings_xhamster.csv", 
@@ -17,16 +18,33 @@ rankings <- read_csv2("analysis/input/rankings_xhamster.csv",
                                     "user_reaction"),
                       skip = 1)
 
+#creating independent tag categories for each case
 tag_matrix <- NULL
+tag_vnames <- NULL
 for(tag in rankings$tag) {
   cat(tag)
   tag_matrix <- cbind(tag_matrix, grepl(tag, porn$channels))
+  vname <- gsub("\\s+",".",tag)
+  vname <- gsub("-","",vname)
+  vname <- gsub("\\+","", vname)
+  tag_vnames <- c(tag_vnames, vname)
+  cat(" ")
+  cat(vname)
   cat("\n")
 }
 
-colnames(tag_matrix) <- rankings$tag
+colnames(tag_matrix) <- tag_vnames
 
-porn <- cbind(porn, tag_matrix)
+porn <- cbind(porn, tag_matrix+1)
+
+library(poLCA)
+
+porn.subsample <- porn[sample(1:nrow(porn), 10000),]
+
+temp <- poLCA(as.formula(paste("cbind(",paste(tag_vnames,collapse=","),")~1",sep="")), 
+              porn.subsample, nclass=10)
+
+summary(porn)
 
 write_csv(porn, "analysis/output/porn_data.csv")
 write_csv(rankings, "analysis/output/rankings_data.csv")
